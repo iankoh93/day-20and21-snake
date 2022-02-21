eedeb3668cae00846f4e8690b6228d775eff813c
iankoh93 <ian_koh93@hotmail.com>
2022-02-21 15:14:16 -0800

    Finished working game

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
+# Set up the game screen
+LIMITS = 300
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=LIMITS*2, height=LIMITS*2)
 screen.bgcolor("black")
 screen.title("Snake Game")
-starting_length = 3
-snakeBody = []
+
 screen.tracer(0)
 game_is_on = True
 
 snake = Snake()
+food = Food()
+score = Scoreboard()
+
+
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.west, key="Left")
+screen.onkey(fun=snake.east, key="Right")
+
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    # Check if there was a collision with food
+    if snake.snakeBody[0].distance(food) < 20:
+        food.refresh()
+        snake.extend()
+        score.current_score += 1
+        score.print_score()
 
+    # Detect collision with wall.
+    if snake.snakeBody[0].xcor() > LIMITS-10 or snake.snakeBody[0].xcor() < -LIMITS+10 or snake.snakeBody[0].ycor() < -LIMITS+10 or snake.snakeBody[0].ycor() > LIMITS-10:
+        food.hideturtle()
+        screen.update()
+        game_is_on = False
+        score.game_over()
 
-
-
-
-
-
-
-
-
-
+    # Detect collision with tail.
+    # If head collides with any segment in tail, game over
+    for segment in snake.snakeBody:
+        if segment == snake.snakeBody[0]:
+            pass
+        elif snake.snakeBody[0].distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,15 +1,17 @@
 from turtle import Turtle
+STEPS = 20
+NORTH = 90
+EAST = 0
+WEST = 180
+SOUTH = 270
 
 
-class Snake:
+class Snake(Turtle):
     def __init__(self, starting_length=3):
+        super().__init__()
         self.snakeBody = []
         for i in range(0, starting_length):
-            new_body = Turtle()
-            new_body.shape("square")
-            new_body.color("white")
-            new_body.penup()
-            self.snakeBody.append(new_body)
+            self.add_segment((0, 0))
             self.snakeBody[i].setpos(x=i * -20, y=0)
 
     def move(self):
@@ -17,5 +19,32 @@ class Snake:
             new_x = self.snakeBody[body - 1].xcor()
             new_y = self.snakeBody[body - 1].ycor()
             self.snakeBody[body].goto(new_x, new_y)
+        self.snakeBody[0].forward(STEPS)
 
-        self.snakeBody[0].forward(20)
+    def add_segment(self, position):
+        # Add new segment to the snake
+        new_body = Turtle()
+        new_body.shape("square")
+        new_body.color("white")
+        new_body.penup()
+        new_body.goto(position)
+        self.snakeBody.append(new_body)
+
+    def extend(self):
+        self.add_segment(self.snakeBody[-1].position())
+
+    def up(self):
+        if self.snakeBody[0].heading() != SOUTH:
+            self.snakeBody[0].setheading(NORTH)
+
+    def down(self):
+        if self.snakeBody[0].heading() != NORTH:
+            self.snakeBody[0].setheading(SOUTH)
+
+    def east(self):
+        if self.snakeBody[0].heading() != WEST:
+            self.snakeBody[0].setheading(EAST)
+
+    def west(self):
+        if self.snakeBody[0].heading() != EAST:
+            self.snakeBody[0].setheading(WEST)
